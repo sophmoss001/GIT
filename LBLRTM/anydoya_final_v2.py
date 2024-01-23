4db53aa7df4a35fa66dae0c76b8cdb56e0f32c0b
Sophie Mosselmans <sm4219@sirocco.sp.ph.ic.ac.uk>
2024-01-23 09:41:19 +0000

    ADDED streching from sanjee

--- LBLRTM/anydoya_final_v2.py
+++ LBLRTM/anydoya_final_v2.py
@@ -7,6 +7,7 @@ from scipy.io import readsav
 import numpy as np
 from matplotlib import pyplot as plt
 import xarray as xr
+import tkinter
 
 """
 20230308_0723_to_0901_calibrated_radiancesv1_1.sav   zenith sky views
@@ -35,7 +36,7 @@ WV              FLOAT     = Array[12500]               (cm-1)
 sav_fname = "/users/jon/data_sets_for_pub_20220323.sav"
 
 data_sav = readsav(sav_fname, verbose=True)
-
+print('data sav worked')
 # times = (data_sav['scan_times'])
 # lucas_type = np.unravel_index((np.abs(times-3600*8)).argmax(), times.shape)
 
@@ -59,7 +60,7 @@ path2 = "/disk1/sm4219/LBLRTM_FOR_SOPHIE/LBLRTM_SIMGA_BOUNDS_40/"
 wn,spectrum = np.loadtxt(path+"apodised_spectra_126_22.txt", unpack = True, dtype=np.float64) #137
 wn2,spectrum2 = np.loadtxt(path2+"apodised_spectra_36_01_12.txt", unpack = True , dtype=np.float64) #40
 
-
+print('found both spectra')
 # wn,spectrum = np.loadtxt(path+'apodised_spectra_126_22.txt', unpack = True, dtype=np.float64)
 # wn2,spectrum2 = np.loadtxt(path2+'apodised_spectra_36_og.txt', unpack = True , dtype=np.float64) #37 LAUYERS
 
@@ -114,36 +115,64 @@ for j in range(0,12500):
 # plt.plot(wn, spectrum_new, label='126 layers', alpha=0.6, lw=0.7, color='darkgreen')
 # plt.plot(wv,rad[0:12500,21, 1, 39], label='data pub', alpha=0.6, lw=0.7, color='red')
 
-# Download Jons data 
+# Download Jons data SIMULATION HIS TAPE 5 MY APODISATION
 path3 = "/disk1/sm4219/LBLRTM_FOR_SOPHIE/LBLRTM_SIMGA_BOUNDS_40/"
 wn3,spectrum3 = np.loadtxt(path2+"apodised_spectra_jon.txt", unpack = True , dtype=np.float64) #jons file
 spectrum_new3 = spectrum3 * 10000
 
+# print('found 3rrd spectr')
+print('len',len(spectrum_new3))
 # Plot jons data ;12500
-plt.plot(wv[0:12500,0], rad[0:12500,31], label='data pub', alpha=0.6, lw=0.7, color='red')
-plt.plot(wn3, spectrum_new3, label='Jons layers', alpha=0.6, lw=0.7, color='darkgreen')
+# plt.plot(wv[0:12500,0], rad[0:12500,31], label='rad from datasetforpub', alpha=0.6, lw=0.7, color='red')
+# plt.plot(wn3, spectrum_new3, label='Jons layers 11', alpha=0.6, lw=0.7, color='darkgreen')
 
-# Plot differeence
+# da3 is the rad which is from dataset pub observationse
 da3 = xr.DataArray(
-    data=spectrum_new3,
-    dims=["wn3"],
+    data=rad[0:12500,31],
+    dims=["wv3"],
     coords=dict(
-        wn3 = np.linspace(300,2000,5574947),
+        wv3 = np.linspace(300,2000,12500),
         # chanigng wavenumber ot be more accurate
     ),)
 
-da_new3 = (da3.interp(wn3=wv[0:12500,0], method="linear", kwargs={"fill_value": "extrapolate"}))
-spec3 = da_new3.data
 
-sub3 = []
-for k in range(0,12500):
-    diff3 = rad[k:k+1,31] - spec3[k:k+1]
-    sub3.append(diff3)
+# apply this to obseravations stretch
+stretch_factor = 1.0016
+wn_stretch = (da3.wv3.values)*stretch_factor
+stretched_obvs = da3.interp(wv3=wn_stretch)
+
+
+da4 = xr.DataArray(
+    data=spectrum_new3,
+    dims=["wv4"],
+    coords=dict(
+	wv4 = np.linspace(300,2000,5574947),
+        # chanigng wavenumber ot be more accurate
+    ))
+
+da_new4 = (da4.interp(wv4=wn_stretch, method="linear", kwargs={"fill_value": "extrapolate"}))
+spec4 = da_new4.data
+
+
+plt.figure()
+
+plt.plot(wn_stretch, stretched_obvs, label='rad from datasetforpub', alpha=0.9, lw=0.9, color='red')
+plt.plot(wn_stretch , spec4, label='apodised spec', alpha=0.9, lw=0.7, color='teal')
+
+
+# sub3 = []
+# for k in range(0,12500):
+#     diff3 = spectrum_new3[k:k+1] - spec3[k:k+1]
+
+#     sub3.append(diff3)
 
 plt.xlim(400,2000)
 plt.ylim(-0.03,0.15)
 
-plt.plot(wv, sub3, label='Difference pub - jons layers', alpha=0.6, lw=0.7, color='teal')
+
+# plt.plot(wv, sub3, 'x', label='Difference pub - jons layers', alpha=0.6, lw=0.7, color='teal')
+
+
 
 # The variable zen is a floating point array fltarr(72,12500) 
 # index 31 (of the index ranging from 0-71) is the 11:00 1 minute average for 11:00
@@ -162,7 +191,6 @@ plt.legend()
 # plt.savefig('wv_rad_andoyda_0803')
 plt.xlabel('Wavenumber / cm-1')
 plt.ylabel('Radiance / [W m-2 sr-1 / cm-1]')
-plt.savefig(path+'andoya_vs_lblrtm_APODISED.png')
-plt.show()
+plt.savefig(path+'jon_andoya_vs_lblrtm_APODISED_NEW.png')
 print('all is dandy')
 
