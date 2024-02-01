48f2cc9031136cfde587902025695ecfbb731de5
Sophie Mosselmans <sm4219@sirocco.sp.ph.ic.ac.uk>
2024-02-01 09:58:50 +0000

    all recent files for better chil plotting etc

--- LBLRTM/anydoya_final_v2.py
+++ LBLRTM/anydoya_final_v2.py
@@ -34,7 +34,6 @@ WV              FLOAT     = Array[12500]               (cm-1)
 
 # File from Jon's publication
 sav_fname = "/users/jon/data_sets_for_pub_20220323.sav"
-
 data_sav = readsav(sav_fname, verbose=True)
 print('data sav worked')
 # times = (data_sav['scan_times'])
@@ -43,16 +42,6 @@ print('data sav worked')
 wv = (data_sav['wv'])
 rad = (data_sav['zen'])
 # rad = (data_sav['radiance'])
-
-# REMOVES ZEROS FROM WV
-# wv_new = wv[wv != 0]
-# print('wavenumbers here', wv_new)
-# plt.title('Difference plot 20230308_8am_radiance')
-# # plt.plot(wv, rad[0:12500,21, 1, 39], color='red')
-# plt.xlabel('Wavenumber(nm)')
-# plt.ylabel('Radiance(W m-2 sr-1 (cm-1)-1)')
-# plt.plot(wv[0:12400], rad[0:12400,21, 1, 39])
-# plt.savefig('wv_rad_andoyda_0803')
 
 path = "/disk1/sm4219/LBLRTM_FOR_SOPHIE/LBLRTM_SIMGA_BOUNDS_40/"
 path2 = "/disk1/sm4219/LBLRTM_FOR_SOPHIE/LBLRTM_SIMGA_BOUNDS_40/"
@@ -83,8 +72,6 @@ da2 = xr.DataArray(
         wn_2 = np.linspace(300,1600,5103796),
     ),)
 
-"""
-
 # interpolating so that wavenumber scales match up
 # da_new = (da.interp(wn=wv, method="linear", kwargs={"fill_value": "extrapolate"}))
 # da_new2 = (da2.interp(wn_2=wv, method="linear", kwargs={"fill_value": "extrapolate"}))
@@ -93,7 +80,7 @@ da2 = xr.DataArray(
 # spec2 = da_new2.data
 # print(len(spec2), da_new2)
 
-"""subtraction
+# subtraction
 sub = []
 for i in range(0,12500):
     diff = rad[i:i+1,21, 1, 39] - spec[i]
@@ -117,80 +104,132 @@ for j in range(0,12500):
 
 # Download Jons data SIMULATION HIS TAPE 5 MY APODISATION
 path3 = "/disk1/sm4219/LBLRTM_FOR_SOPHIE/LBLRTM_SIMGA_BOUNDS_40/"
-wn3,spectrum3 = np.loadtxt(path2+"apodised_spectra_jon.txt", unpack = True , dtype=np.float64) #jons file
+wn3,spectrum3 = np.loadtxt(path2+"apodised_spectra_NEW_SOPH.txt", unpack = True , dtype=np.float64) #jons file
 spectrum_new3 = spectrum3 * 10000
 
-# print('found 3rrd spectr')
-print('len',len(spectrum_new3))
-# Plot jons data ;12500
-# plt.plot(wv[0:12500,0], rad[0:12500,31], label='rad from datasetforpub', alpha=0.6, lw=0.7, color='red')
-# plt.plot(wn3, spectrum_new3, label='Jons layers 11', alpha=0.6, lw=0.7, color='darkgreen')
+# wn3,spectrum3 = np.loadtxt(path2+"apodised_spectra_NEW_old.txt.txt", unpack = True , dtype=np.float64) #jons file
+# np.savetxt(path+'/apodised_spectra_NEW_NEW.txt',np.vstack([apodised_wn, apodised_spectrum]).T)
+#np.savetxt(path+'/apodised_spectra_NEW_old.txt',np.vstack([wn,apodised_interp]).T)
+
+# TRYING WITH JONS OWN APODISATION
+# File from Jon's publication
+sav_fname = "/disk1/sm4219/GIT/LBLRTM/example_spe.sav"
+data_sav_jon = readsav(sav_fname, verbose=True)
+# print('data sav 2 worked')
+# times = (data_sav['scan_times'])
+# lucas_type = np.unravel_index((np.abs(times-3600*8)).argmax(), times.shape)
+wv_jon = (data_sav_jon['conv_wv'])
+rad_jon = (data_sav_jon['con_spe'])
+spe_jon = (data_sav_jon['spe'])
+# print(spe_jon)
 
 # da3 is the rad which is from dataset pub observationse
 da3 = xr.DataArray(
     data=rad[0:12500,31],
     dims=["wv3"],
     coords=dict(
-        wv3 = np.linspace(300,2000,12500),
+        wv3 = np.concatenate(wv, axis=0)
         # chanigng wavenumber ot be more accurate
     ),)
 
 
 # apply this to obseravations stretch
-stretch_factor = 1.0016
+stretch_factor = 1.00016
 wn_stretch = (da3.wv3.values)*stretch_factor
 stretched_obvs = da3.interp(wv3=wn_stretch)
 
+# print(stretched_obvs)
 
 da4 = xr.DataArray(
     data=spectrum_new3,
     dims=["wv4"],
     coords=dict(
-	wv4 = np.linspace(300,2000,5574947),
+	wv4 = np.linspace(300,2000,8499),
         # chanigng wavenumber ot be more accurate
     ))
 
-da_new4 = (da4.interp(wv4=wn_stretch, method="linear", kwargs={"fill_value": "extrapolate"}))
-spec4 = da_new4.data
+wn_refined = np.linspace(400,1600,120000)
+# wn_refined = np.linspace(400,2000,120000)
+
+da_new4 = (da4.interp(wv4=wn_refined))
+da_new5 = (da_new4.interp(wv4=wn_stretch))
+spec4 = da_new5.data
 
 
 plt.figure()
+# plt.plot(wn_stretch, ((stretched_obvs*10000)/10), label='rad from datasetforpub', alpha=0.9, lw=0.6, color='red')
+# plt.plot(wn_stretch, (spec4/10), label='spec thru soph layers', alpha=0.6, lw=0.7, color='green')
+# plt.plot(wv_jon, (rad_jon)*1000, label='Jons layers 11', alpha=0.6, lw=0.7, color='blue')
 
-plt.plot(wn_stretch, stretched_obvs, label='rad from datasetforpub', alpha=0.9, lw=0.9, color='red')
-plt.plot(wn_stretch , spec4, label='apodised spec', alpha=0.9, lw=0.7, color='teal')
+da6 = xr.DataArray(
+    data=rad_jon,
+    dims=["wv6"],
+    coords=dict(
+	wv6 = np.linspace(300,2000,8500),
+        # chanigng wavenumber ot be more accurate
+    ))
+
+da_new6 = (da6.interp(wv6=wn_refined))
+da_new7 = (da_new6.interp(wv6=wn_stretch))
+spec6 = da_new7.data
+
+print(len(spec4))
+
+# plt.plot(wn_stretch, (spec6)*1000, label='jon rad my strecth ', alpha=0.6, lw=0.7, color='darkblue')
+
+# plt.plot(wn_stretch, (stretched_obvs*10000)/10, label='rad from datasetforpub', alpha=0.9, lw=0.6, color='red')
+# plt.plot(wn_stretch, (spec4)/10, label='soph layers', alpha=0.6, lw=0.9, color='green')
+
+# plt.plot(wn_stretch , spec4*1000, label='apodised spec', alpha=0.9, lw=0.6, color='teal')
+# plt.plot(wn_stretch, stretched_obvs*1000, label='rad from datasetforpub', alpha=0.9, lw=0.6, color='red')
+
+# plt.plot(wv_jon, (rad_jon)*10000, label='Jons layers 11', alpha=0.6, lw=0.7, color='blue')
+# plt.plot(wv[0:12500,0], rad[0:12500,31], label='rad from datasetforpub', alpha=0.6, lw=0.7, color='red')
+# plt.plot(wn3, spectrum_new3, label='raw sophiejon', alpha=0.6, lw=0.7, color='green')
+# plt.plot(wv_jon, spe_jon*10000, color='red', lw=0.5, alpha=0.6)
+
+plt.xlim(400,1600)
+plt.ylim(-8,8)
 
 
-# sub3 = []
-# for k in range(0,12500):
-#     diff3 = spectrum_new3[k:k+1] - spec3[k:k+1]
+stretch_new = stretched_obvs[2000:10500]
+print(stretch_new)
 
-#     sub3.append(diff3)
+print(len(wn_stretch))
+print(wn_stretch[2000:])
+# DIFFERENCE CALCULATIONS AND PLOTS
+sub3 = ((stretch_new*10000)/10) - (spec4/10)[2000:10500]
+plt.plot(wn_stretch[2000:10500],sub3, label='Difference pub - jons layers', alpha=0.8, lw=0.4, color='pink',)
 
-plt.xlim(400,2000)
-plt.ylim(-0.03,0.15)
+# plt.plot(wn_stretch[2000:10500],(stretch_new*10000)/10, label='stretchnew', lw=0.6)
+
+# plt.plot(wn_stretch[2000:10500], (spec4/10)[2000:10500], label='jonlayers', lw=0.6)
 
 
-# plt.plot(wv, sub3, 'x', label='Difference pub - jons layers', alpha=0.6, lw=0.7, color='teal')
+# plt.plot(wv_jon,(spec4)*1000, color='blue')
 
 
+# sub5 = ((stretch_new*10000)/10) - (rad_jon)*1000
+# plt.plot(wv_jon, sub5, label='Difference pub - jons layers', alpha=0.8, lw=0.4, color='teal')
 
 # The variable zen is a floating point array fltarr(72,12500) 
 # index 31 (of the index ranging from 0-71) is the 11:00 1 minute average for 11:00
 
-# ax1.plot(wv, rad[0:12500,0,0,0])
-# ax1.set_xlim(400,1500)
-# ax1.set_ylim(0,0.12)
-# ax1.set_title('Downwelling Spectrum')
-# ax1.set_xlabel('Wavenumber / cm-1')
-# ax1.set_ylabel('Radiance / [W m-2 sr-1 / cm-1]')
-# ax2.plot(dum_new_fre, diff_dum, lw=0.8, color = 'purple', label='36-126')
-# ax2.set_title('Difference')
-# ax2.set_xlabel('Wavenumber / cm-1')
-# ax2.set_ylabel('Radiance / [W m-2 sr-1 / cm-1]')
 plt.legend()
-# plt.savefig('wv_rad_andoyda_0803')
 plt.xlabel('Wavenumber / cm-1')
-plt.ylabel('Radiance / [W m-2 sr-1 / cm-1]')
+plt.ylabel('Radiance / [mW m-2 sr-1 / cm-1]')
 plt.savefig(path+'jon_andoya_vs_lblrtm_APODISED_NEW.png')
-print('all is dandy')
+
+"""
+ax1.plot(wv, rad[0:12500,0,0,0])
+ax1.set_xlim(400,1500)
+ax1.set_ylim(0,0.12)
+ax1.set_title('Downwelling Spectrum')
+ax1.set_xlabel('Wavenumber / cm-1')
+ax1.set_ylabel('Radiance / [W m-2 sr-1 / cm-1]')
+ax2.plot(dum_new_fre, diff_dum, lw=0.8, color = 'purple', label='36-126')
+ax2.set_title('Difference')
+ax2.set_xlabel('Wavenumber / cm-1')
+ax2.set_ylabel('Radiance / [W m-2 sr-1 / cm-1]')
+"""
 
--- plot_era5_chil_trial.py
+++ plot_era5_chil_trial.py
@@ -8,17 +8,31 @@ import matplotlib.dates as mdates
 from netCDF4 import Dataset
 from matplotlib.dates import DateFormatter
 import matplotlib as mpl
+from datetime import datetime
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+from matplotlib.colors import ListedColormap
+
 
 # importing data set
-ds_all = xarray.load_dataset('download.nc', engine="netcdf4")
+ds_1part = xarray.load_dataset('/home/sm4219/download_new_23_lateryears.nc', engine="netcdf4")
 
-updated_ds = ds_all.sel(latitude = 51.145 , longitude = -1.44, method = 'nearest')
-total_clouds = (updated_ds.variables['tcc'])
+ds_2part = xarray.load_dataset('/home/sm4219/download_new_23.nc', engine="netcdf4")
+
+updated_ds_1 = ds_1part.sel(latitude = 51.145 , longitude = -1.44, method = 'nearest')
+updated_ds_2 = ds_2part.sel(latitude = 51.145 , longitude = -1.44, method = 'nearest')
+
+ds_grid = [updated_ds_1 ,updated_ds_2]
+updated_ds = xarray.combine_by_coords(ds_grid, combine_attrs='override')
+
+print(updated_ds['time'])
+
+# total_clouds = (updated_ds.variables['tcc'])
 total_column_w = (updated_ds.variables['tcwv'])
 low_clouds = (updated_ds.variables['lcc'])
 high_clouds = (updated_ds.variables['hcc'])
 timew = (updated_ds.variables['time'])
-
+# raise Exception('HOLD YOUR HORSES')
 # print(total_clouds)
 # fig, ax1 = plt.subplots(figsize=(12, 8))
 # # bins = np.arange(1,10,1)
@@ -31,26 +45,12 @@ timew = (updated_ds.variables['time'])
 # ax2.plot(timew, total_column_w,'o', color='salmon', label='total column wv',)
 
 total_column_w2 = np.array(total_column_w)
-total_clouds2 = np.array(total_clouds)
+# total_clouds2 = np.array(total_clouds)
+
 timew2 = np.array(timew)
 low_clouds2 = np.array(low_clouds)
 high_clouds2 = np.array(high_clouds)
 
-# mean
-
-n_a = 12
-
-mean_cloud = np.mean(total_clouds2.reshape(-1,n_a), axis =1)
-mean_wv = np.mean(total_column_w2.reshape(-1,n_a), axis =1)
-mean_low = np.mean(low_clouds2.reshape(-1,n_a), axis =1)
-mean_high = np.mean(high_clouds2.reshape(-1,n_a), axis =1)
-
-# mean_time = np.mean(timew2.reshape(-1,n_a), axis =1)
-
-# time_array = xarray.IndexVariable((np.array(timew)))
-
-# months = timew2.to_series().dt.month
-# months = np.datetime_as_string(timew2, unit='M').astype('int')]
 months = []
 
 for i in range(len(timew2)):
@@ -60,20 +60,16 @@ years = []
 for j in range(len(timew2)):
     years.append(int(np.datetime_as_string(timew2[j], unit='Y').split('-')[0]))
 
-# fig4, ax4 = plt.subplots(figsize=(12, 8))
-
-ax4.set_title('2012 to 2012')
-ax4.set_ylabel('cloud cover')
-ax4.set_xlabel('tcwv')
-
-# plt.scatter(total_column_w, total_clouds, c=months, cmap='viridis')
-
+# ax4.set_title('2012 to 2012')
+# ax4.set_ylabel('cloud cover')
+# ax4.set_xlabel('tcwv')
 
 # # Create xarray Dataset
 ds = xarray.Dataset({
     'total_column_w': ('time', total_column_w),
     'high_cloud': ('time', high_clouds),
-    'total_clouds': ('time', total_clouds),
+    # 'total_clouds': ('time', total_clouds),
+    'low_cloud': ('time', low_clouds),
     'years': ('time', years),
     'months': ('time', months)
 }, coords={'time': timew2})
@@ -94,9 +90,110 @@ ds['year_month'] = ds['years'] * 100 + ds['months']
 grouped_data2 = ds2.groupby('year_month').mean(dim='time')
 grouped_data = ds.groupby('year_month').mean(dim='time')
 
-cmap = plt.get_cmap('Paired', 12)
-cmap.set_under('gray')
+# cmap = plt.get_cmap('Paired', 12)
+# cmap.set_under('gray')
 
+"12 SUBPLOT START HERE"
+
+# Create 12 subplots
+fig, axs = plt.subplots(3, 4, figsize=(22, 18), sharex=True, sharey=True)
+
+# Flatten the 2D array of subplots to make indexing easier
+axs = axs.flatten()
+
+# Define a list of distinct colors
+distinct_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+                   '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+                   '#ff0000', '#00ff00']  # Add or modify colors as needed
+
+# Create a custom ListedColormap with the distinct colors
+color_map = ListedColormap(distinct_colors)
+
+# Corrected list with distinct colors
+distinct_colors = ['#ff0000', '#ff9900', '#ffff00', '#00ff00', '#00ccff',
+                   '#3300cc', '#9900ff', '#ff00cc', '#006400', '#ff6699',
+                   '#666666', '#996633']  # Dark green: '#006400', Light pink: '#ffc0cb'
+
+
+# Create a custom ListedColormap with the different distinct colors
+color_map2  = ListedColormap(distinct_colors )
+
+# Create year_month variable in the original dataset
+ds['year_month'] = ds['years'] * 100 + ds['months']
+
+print(ds['years'].values)
+
+# Iterate over each month
+months_range = range(1, 13)
+
+for i, month in enumerate(months_range):
+    # Select data for the current month
+    # monthly_data = ds.where(ds['months'] == month, drop=True)3
+    # print(i, monthly_data)
+    # Group by year_month and calculate the mean
+    # grouped_data2 = ds.groupby('year_month').mean(dim='time')
+
+    # Select data for the current month
+    monthly_data = ds.where(ds['months'] == month, drop=True)
+
+    grouped_data = monthly_data.groupby('year_month').mean(dim='time')
+
+
+    # Normalize 'months' values to be between 0 and 1
+    normalized_months = (grouped_data['months'] - grouped_data['months'].min()) / (grouped_data['months'].max() - grouped_data['months'].min())
+
+
+    # Filter data based on conditions
+    grouped_data = grouped_data.where((grouped_data['low_cloud'] < 0.4) & (grouped_data['total_column_w'] < 20))
+
+    # Scatter plot, c=color_map(i/12))
+    axs[i].scatter(grouped_data['total_column_w'], grouped_data['high_cloud'], s=220, c=distinct_colors[i], alpha=0.8 )
+    axs[i].scatter(grouped_data['total_column_w'], grouped_data['low_cloud'], s=220, marker='^', c=distinct_colors[i], alpha=0.8)
+
+    # Set subplot title with month names
+    month_name = datetime.strptime(str(month), "%m").strftime("%b")
+    axs[i].set_title(month_name, fontsize=26)
+
+     # Increase the size of axis labels and tick labels
+    axs[i].tick_params(axis='both', which='both', labelsize=16)
+
+    
+    
+# Set common labels and title
+fig.suptitle('Total Column Water vs Total Clouds for Each Month')
+fig.text(0.5, 0.04, 'Total Column Water', ha='center', va='center')
+fig.text(0.07, 0.5, 'Total Clouds', ha='center', va='center', rotation='vertical')
+
+
+# Create custom legend with markers
+legend_elements = [Line2D([0], [0], marker='o', color='w', markerfacecolor=color_map(0), markersize=10, label='High Clouds'),
+                    Line2D([0], [0], marker='^', color='w', markerfacecolor=color_map2(0), markersize=10, label='Low Clouds')]
+
+# Add the custom legend to the figure
+fig.legend(handles=legend_elements, loc='upper right', title='Cloud Types')
+
+# Create a colorbar axis on the right side of the figure
+cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])  # Adjust position and size as needed
+
+# Add a colorbar
+cbar = fig.colorbar(axs[0].scatter([], [], c=[], cmap=color_map2, vmin=1, vmax=12), cax=cbar_ax, label='Month')
+
+# Create a colorbar axis on the right side of the figure
+# cbar_ax2 = fig.add_axes([0.96, 0.15, 0.02, 0.7])  # Adjust position and size as needed
+
+# Add a colorbar
+# cbar2 = fig.colorbar(axs[0].scatter([], [], c=[], cmap=color_map, vmin=1, vmax=12), cax=cbar_ax2, label='Month')
+
+
+# Customize colorbar ticks and tick labels to represent months
+cbar.set_ticks(months_range)
+cbar.set_ticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+plt.savefig('/home/sm4219/twelveplots_newjan.png')
+
+# Adjust layout
+# plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+"END HERE"
 # create a second axes for the colorbar
 # Create a scatter plot with color based on months
 # plt.scatter(grouped_data['total_column_w'], grouped_data['high_cloud'], c=grouped_data['months'], 
@@ -128,7 +225,26 @@ cmap.set_under('gray')
 # ax4.xaxis.set_major_locator(mdates.WeekdayLocator(interval=50))
 # plt.xlabel('Day-Month-Year')
 
-plt.savefig('high_clouds_allyears.png')
 
 print('booya')
 # hea
+
+"""
+
+# Iterate over each month
+for i, month in enumerate(range(1, 13)):
+    # Select data for the current month
+    monthly_data = ds.where(ds['months'] == month, drop=True)
+    
+    # Group by year_month and calculate the mean
+    grouped_data = monthly_data.groupby('year_month').mean(dim='time')
+    
+
+    # Scatter plot
+    axs[i].scatter(grouped_data['total_column_w'], grouped_data['high_cloud'], s=50, c=color_map(i/12))
+    axs[i].scatter(grouped_data['total_column_w'], grouped_data['low_cloud'], s=50, marker='^', c=color_map2(i/12))
+    # Set subplot title with month names
+    month_name = datetime.strptime(str(month), "%m").strftime("%b")
+    axs[i].set_title(month_name)
+
+"""
